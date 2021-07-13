cba07297fdbe1ed244974e596a0e72a372b73d29
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-07-13 01:02:16 -0700

    Add version for help center (#70)
    * Add version for help center
    
    * Review changes
    
    * Review changes 2

--- lib/testing-helpcenter/config.rb
+++ lib/testing-helpcenter/config.rb
@@ -16,8 +16,18 @@ module TestingHelpCentreOnlyoffice
       @browser = params.fetch(:browser, :chrome)
     end
 
-    def version
-      Time.new.strftime('%d/%m/%Y')
+    # @return [String] hash of site by url `help_center/revision or test run date`
+    def help_center_version
+      version_uri = URI("#{@server}/revision")
+      revision_data = Net::HTTP.get(version_uri)
+      if revision_data == "changelog.xml doesn't exist"
+        Time.new.strftime('%d/%m/%Y')
+      else
+        revision_data.split[0]
+      end
+    rescue StandardError => e
+      OnlyofficeLoggerHelper.log("Cannot get site hash because of #{e}")
+      'unknown'
     end
 
     private
