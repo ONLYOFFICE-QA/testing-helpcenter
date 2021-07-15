fae32b821553e76d3f2b98c54d891015c7cfce32
Olga Nesterova <olganesterova034@gmail.com>
2021-07-15 02:45:49 -0700

    Delete before all

--- spec/functional/helpcenter_main_page_faq_links_spec.rb
+++ spec/functional/helpcenter_main_page_faq_links_spec.rb
@@ -2,29 +2,28 @@
 
 require 'spec_helper'
 test_manager = TestingHelpCentreOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+help_center_home_page, test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config)
 
 describe 'Help center FAQ links' do
-  before(:all) { @help_center_home_page, @test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config) }
-
   after do |example|
-    test_manager.add_result(example, @test)
+    test_manager.add_result(example, test)
   end
 
-  after(:all) { @test.webdriver.quit }
+  after(:all) { test.webdriver.quit }
 
   TestingHelpCentreOnlyoffice::HelpCenterData.main_page_faq_links.each do |question_title, answer_links|
     describe question_title.to_s do
-      before { @help_center_home_page.open_main_page_faq(question_title) }
+      before { help_center_home_page.open_main_page_faq(question_title) }
 
       answer_links.each do |answer_link|
         it "`#{answer_link}` link of `#{question_title}` faq works" do
-          expect(@help_center_home_page).to be_help_center_faq_answer_link_alive(question_title, answer_link)
+          expect(help_center_home_page).to be_help_center_faq_answer_link_alive(question_title, answer_link)
         end
       end
     end
   end
 
   it "Main page FAQ questions number didn't change" do
-    expect(@help_center_home_page.faq_number).to eq(TestingHelpCentreOnlyoffice::HelpCenterData.main_page_faq_links.count)
+    expect(help_center_home_page.faq_number).to eq(TestingHelpCentreOnlyoffice::HelpCenterData.main_page_faq_links.count)
   end
 end
