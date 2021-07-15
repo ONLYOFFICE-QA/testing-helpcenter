a3254be252138d1328029c4d1f5c2eb11fc39c8f
Olga Nesterova <40513035+olga-nesterova@users.noreply.github.com>
2021-07-15 03:10:33 -0700

    Fix adding tests to test manager (#74)
    * Fix adding tests to test manager
    
    * Delete before all

--- spec/functional/helpcenter_footer_links_spec.rb
+++ spec/functional/helpcenter_footer_links_spec.rb
@@ -2,20 +2,20 @@
 
 require 'spec_helper'
 test_manager = TestingHelpCentreOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+help_center_home_page, test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config)
 
 describe 'Help center footer links' do
-  before(:all) { @help_center_home_page, @test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config) }
-
-  after :all do |example|
-    test_manager.add_result(example, @test)
-    @test.webdriver.quit
+  after do |example|
+    test_manager.add_result(example, test)
   end
+
+  after(:all) { test.webdriver.quit }
 
   TestingHelpCentreOnlyoffice::HelpCenterData.footer_links.each do |section_title, titles|
     titles.each do |title|
       it "`#{title}` link of `#{section_title}` footer section works" do
         pending('Link `https://www.instagram.com/the_onlyoffice/` answered with 405') if title == 'Follow us on Instagram'
-        expect(@help_center_home_page).to be_help_center_footer_link_alive(section_title, title)
+        expect(help_center_home_page).to be_help_center_footer_link_alive(section_title, title)
       end
     end
   end
--- spec/functional/helpcenter_main_page_links_spec.rb
+++ spec/functional/helpcenter_main_page_links_spec.rb
@@ -2,19 +2,19 @@
 
 require 'spec_helper'
 test_manager = TestingHelpCentreOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+help_center_home_page, test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config)
 
 describe 'Help center main page links' do
-  before(:all) { @help_center_home_page, @test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config) }
-
-  after :all do |example|
-    test_manager.add_result(example, @test)
-    @test.webdriver.quit
+  after do |example|
+    test_manager.add_result(example, test)
   end
+
+  after(:all) { test.webdriver.quit }
 
   describe 'Links under search field' do
     TestingHelpCentreOnlyoffice::HelpCenterData.main_page_links.each do |title|
       it "`#{title}` link under search field works" do
-        expect(@help_center_home_page).to be_help_center_links_under_search_field_alive(title)
+        expect(help_center_home_page).to be_help_center_links_under_search_field_alive(title)
       end
     end
   end
@@ -22,12 +22,12 @@ describe 'Help center main page links' do
   describe 'Block links' do
     TestingHelpCentreOnlyoffice::HelpCenterData.main_page_block_links.each do |header_title, body_titles|
       it "Block `#{header_title}` header link works" do
-        expect(@help_center_home_page).to be_help_center_block_header_link_alive(header_title)
+        expect(help_center_home_page).to be_help_center_block_header_link_alive(header_title)
       end
 
       body_titles.each do |body_title|
         it "`#{body_title}` link of `#{header_title}` block works" do
-          expect(@help_center_home_page).to be_help_center_block_body_link_alive(header_title, body_title)
+          expect(help_center_home_page).to be_help_center_block_body_link_alive(header_title, body_title)
         end
       end
     end
