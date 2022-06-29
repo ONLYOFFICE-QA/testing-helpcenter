fc6ca70d4666253110e36973e954d3bb7837c7f3
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-06-29 15:06:14 +0500

    Add JSON to store page data title

--- lib/testing-helpcenter/data/page_title_list.rb
+++ lib/testing-helpcenter/data/page_title_list.rb
@@ -5,7 +5,7 @@ require 'json'
 class PageTitleList
   attr_reader :hash
 
-  def initialize( file_path )
+  def initialize(file_path)
     @raw_data = File.read(file_path)
     @hash = JSON.parse(@raw_data, { symbolize_names: true})
   end
