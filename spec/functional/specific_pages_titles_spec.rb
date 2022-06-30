a3773d5b44cf6ed6502b5d989ef613fd568ba5ab
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-06-30 19:02:56 +0500

    Add link from issue#218
    Implement #218

--- spec/functional/specific_pages_titles_spec.rb
+++ spec/functional/specific_pages_titles_spec.rb
@@ -9,6 +9,7 @@ page_hash = PageTitleList.new
 describe 'Help center footer links' do
   before do
     _help_center_home_page, test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config)
+
   end
 
   after do |example|
