0bc7a689bf7ed9b5fefc099fc7adf4f7d750ff30
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-06-30 16:36:19 +0500

    Add links from Issue #399 to JSON
    Implement #399

--- spec/functional/specific_pages_titles_spec.rb
+++ spec/functional/specific_pages_titles_spec.rb
@@ -5,11 +5,8 @@ require_relative '../../lib/testing-helpcenter/data/page_title_list'
 
 test_manager = TestingHelpCentreOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
 test = nil
-<<<<<<< HEAD
+
 page_hash = PageTitleList.new
-=======
-page_hash = PageTitleList.new('lib/testing-helpcenter/data/page_titles.json')
->>>>>>> 960e86f (Add JSON)
 
 describe 'Help center footer links' do
   before do
@@ -23,17 +20,10 @@ describe 'Help center footer links' do
   end
 
   page_hash.hash.each do |hash|
-<<<<<<< HEAD
     it "Check page title of '#{hash[:url]}'" do
       page_to_open = "#{test.webdriver.get_url}#{hash[:url]}"
       test.webdriver.open(page_to_open)
       expect(test.webdriver.get_title_of_current_tab).to eq((hash[:title]))
-=======
-    it "check page title of #{hash[:url]}" do
-      page_to_open = "#{test.webdriver.get_url}#{hash[:url]}"
-      test.webdriver.open(page_to_open)
-      expect(test.webdriver.get_title_of_current_tab).to eq((hash[:title]).to_s)
->>>>>>> 960e86f (Add JSON)
     end
   end
 end
