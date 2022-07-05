be37f2cc9f48f7ece793ae3fe6a8ef8d6e58759f
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2022-07-05 19:52:18 +0500

    Add spec for hidden tags (#432)
    * Add spec for hidden tags
    
    * Add spec for pop up tags
    
    * Add spec for pop up tags
    
    * Add spec for pop up tags
    
    * Add spec for pop up tags

--- lib/testing-helpcenter/help_center_tags.rb
+++ lib/testing-helpcenter/help_center_tags.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'help_center_tags/articles_with_tag_popup'
+
 # Methods to work with tag list on https://helpcenter.onlyoffice.com/tags.aspx
 class HelpCenterTags
   include PageObject
@@ -7,10 +9,18 @@ class HelpCenterTags
   def initialize(instance)
     super(instance.webdriver.driver)
     @instance = instance
-    @xpath = "//div[contains(@class, 'div_for_index')]//a[contains(@class, 'text-container-link')]"
+    @tags_xpath = "//div[contains(@class, 'div_for_index')]//a[contains(@class, 'text-container-link')]"
   end
 
+  # @return [Array<String>] list of tags in tags page
   def tags_list
-    @instance.webdriver.get_text_of_several_elements(@xpath)
+    @instance.webdriver.get_text_of_several_elements(@tags_xpath)
+  end
+
+  def tag_click(tag_name)
+    tags_list_from_data = TagsListFromData.new
+    tag_index = tags_list_from_data.tags.find_index(tag_name)
+    @instance.webdriver.click_on_locator("(#{@tags_xpath})[#{tag_index + 1}]")
+    ArticlesWithTagPopup.new(@instance)
   end
 end
---
+++ lib/testing-helpcenter/help_center_tags/articles_with_tag_popup.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+# Methods to work with Pop up tags at https://helpcenter.onlyoffice.com/tags.aspx
+class ArticlesWithTagPopup
+  include PageObject
+
+  def initialize(instance)
+    super(instance.webdriver.driver)
+    @instance = instance
+    @pop_up_tags_xpath = "//div[@id = 'textHelpContent']//a"
+  end
+
+  # @return [Array<String>] list of tags in pop up window
+  def pop_up_tags_list
+    @instance.webdriver.get_text_of_several_elements(@pop_up_tags_xpath)
+  end
+end
