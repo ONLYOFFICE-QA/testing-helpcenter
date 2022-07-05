dc162d6b63798236458f1b25323def4f10ad6163
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-07-05 15:49:20 +0500

    Add spec for hidden tag s

--- lib/testing-helpcenter/help_center_tags.rb
+++ lib/testing-helpcenter/help_center_tags.rb
@@ -7,10 +7,20 @@ class HelpCenterTags
   def initialize(instance)
     super(instance.webdriver.driver)
     @instance = instance
-    @xpath = "//div[contains(@class, 'div_for_index')]//a[contains(@class, 'text-container-link')]"
+    @tags_xpath = "//div[contains(@class, 'div_for_index')]//a[contains(@class, 'text-container-link')]"
+    @hidden_tags_xpath = "#{@tags_xpath}//div[contains(@class, 'text-container-content')]//a"
   end
 
   def tags_list
-    @instance.webdriver.get_text_of_several_elements(@xpath)
+    @instance.webdriver.get_text_of_several_elements(@tags_xpath)
+  end
+
+  def hidden_tags_list
+    @instance.webdriver.get_text_of_several_elements(@hidden_tags_xpath)
+  end
+
+  def tag_click(tag_name, tags_array)
+    tag_index = (tags_array.find_index(tag_name) + 1).to_s
+    @instance.webdriver.get_element("//div[contains(@class, 'div_for_index')]//li[#{tag_index}]//a[contains(@class, 'text-container-link')]").click
   end
 end
