731a9c8ecf67aaf4bf6d8e316218859e67a0aae9
Kobiljon Komilov <38238032+Qobiljonn@users.noreply.github.com>
2022-07-08 13:50:53 +0500

    Add spec for tags articles (#438)
    * Add spec for tags articles
    
    * Add spec for tags articles
    
    * Add spec for tags articles
    
    * Add spec for tags articles

--- spec/functional/all_tags_articles_check_spec.rb
+++ spec/functional/all_tags_articles_check_spec.rb
@@ -6,7 +6,7 @@ test_manager = TestingHelpCentreOnlyoffice::TestManager.new(suite_name: File.bas
 test = nil
 tags_list_from_data = TagsListFromData.new
 
-describe 'Help center pop up tags' do
+describe 'Check articles for each tag' do
   before do
     @help_center_home_page, test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config)
   end
@@ -16,10 +16,11 @@ describe 'Help center pop up tags' do
     test.webdriver.quit
   end
 
-  it 'Closes pop up tag window' do
-    tags_page = @help_center_home_page.open_tags_page
-    pop_up_tag_window = tags_page.tag_click(tags_list_from_data.tags.first)
-    pop_up_tag_window.close
-    expect(pop_up_tag_window).not_to be_opened
+  tags_list_from_data.tags.each do |tag|
+    it "Check pop up for tag: '#{tag}'" do
+      tags_page = @help_center_home_page.open_tags_page
+      pop_up_tag_window = tags_page.tag_click(tag)
+      expect(pop_up_tag_window.pop_up_tags_list).not_to be_empty
+    end
   end
 end
