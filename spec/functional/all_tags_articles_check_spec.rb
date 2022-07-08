875d75ca84676af8b716b11ce7dac8657dd3bd33
Kobiljon Komilov <qobiljon3108@gmail.com>
2022-07-08 13:24:03 +0500

    Add spec for tags articles

---
+++ spec/functional/all_tags_articles_check_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+test_manager = TestingHelpCentreOnlyoffice::TestManager.new(suite_name: File.basename(__FILE__))
+test = nil
+tags_list_from_data = TagsListFromData.new
+
+describe 'Help center hidden tags' do
+  before do
+    @help_center_home_page, test = TestingHelpCentreOnlyoffice::HelpCenterHelper.new.open_help_center_main_page(config)
+  end
+
+  after do |example|
+    test_manager.add_result(example, test)
+    test.webdriver.quit
+  end
+
+  tags_list_from_data.tags.each do |tag|
+    it "Check pop up for tag: '#{tag}'" do
+      tags_page = @help_center_home_page.open_tags_page
+      pop_up_tag_window = tags_page.tag_click(tag)
+      expect(pop_up_tag_window.pop_up_tags_list).not_to be_empty
+    end
+  end
+end
